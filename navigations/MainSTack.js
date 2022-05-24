4cf158710c768f95559f00b013029d8d09f512c5
Russell Quaicoo <russellquaicoo1@gmail.com>
2022-05-24 02:18:33 +0000

    application stack updated

--- navigations/MainSTack.js
+++ navigations/MainSTack.js
@@ -8,6 +8,9 @@ import ProfileScreen from '../Screens/ProfileScreen';
 import OTP from '../Screens/OTP';
 import Tabs from './Tabs';
 import Home from '../Screens/Home';
+import History from '../Screens/History';
+import Details from '../Screens/Details';
+import Cart from '../Screens/Cart';
 
 const Stack = createNativeStackNavigator();
 
@@ -38,12 +41,17 @@ const ProfileStack = () => {
 
 export default function Mainstack() {
   return (
+
     <Stack.Navigator screenOptions={{headershown: false}} >
         <Stack.Screen  name="Startup" component={Startup} options={{ headerShown: false }}/>
         <Stack.Screen  name="Login" component={Login} options={{ headerShown: false }} />
         <Stack.Screen  name="Register" component={Register} options={{ headerShown: false }} /> 
         <Stack.Screen  name="OTP" component={OTP} options={{ headerShown: false }} /> 
+        <Stack.Screen  name="Cart" component={Cart} options={{ headerShown: false }} />
+        <Stack.Screen name="Details" component={Details} options={{ headerShown: false }}/>
+        <Stack.Screen name="History" component={History} options={{ headerShown: false }}/>
         <Stack.Screen  name="Home" component={Home} options={{ headerShown: false }} />
+        
         <Stack.Screen  name="TabNav" component={Tabs} />
     </Stack.Navigator>
   );
