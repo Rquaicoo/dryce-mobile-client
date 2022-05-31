b9356275f616ad04dce313f7187be1bfc37ddae7
unknown <agocoded@gmail.com>
2022-05-31 15:56:00 +0000

    Tab Error Fixed

--- navigations/MainSTack.js
+++ navigations/MainSTack.js
@@ -29,19 +29,13 @@ const screenOptionstyle = {
 
 const HomeStack = () => {
     return(
-<<<<<<< HEAD
+
         <View style={{flex: 1}} collapsable={false} >
             <Stack.Navigator screenOptions={screenOptionstyle}>
                 <Stack.Screen  name="HomeScreen" component={Home} />
             </Stack.Navigator>
         </View>
-=======
-        <Stack.Navigator screenOptions={screenOptionstyle}>
 
-            <Stack.Screen  name="Home" component={Home} />
-
-        </Stack.Navigator>
->>>>>>> 3a2a7be96ec24540a411ef6c24cc7f838d75fcb2
     );
 }
 
@@ -57,35 +51,23 @@ const ProfileStack = () => {
 
 const OrdersStack = () => {
     return(
-<<<<<<< HEAD
+
         <View style={{flex: 1}} collapsable={false} >
             <Stack.Navigator screenOptions={screenOptionstyle} >
                 <Stack.Screen  name="Orders" component={Orders} />
             </Stack.Navigator>
         </View>
-=======
-        <Stack.Navigator screenOptions={screenOptionstyle} >
-            <Stack.Screen  name="Checkout" component={Checkout}/>
-            <Stack.Screen  name="Cart" component={Cart} />
-            <Stack.Screen name="Details" component={Details} />
-        </Stack.Navigator>
->>>>>>> 3a2a7be96ec24540a411ef6c24cc7f838d75fcb2
     );
 }
 
 const HistoryStack = () => {
     return(
-<<<<<<< HEAD
+
         <View style={{flex: 1}} collapsable={false} >
             <Stack.Navigator screenOptions={screenOptionstyle} >
                 <Stack.Screen  name="Historys" component={History} />
             </Stack.Navigator>
         </View>
-=======
-        <Stack.Navigator screenOptions={screenOptionstyle} >
-            <Stack.Screen  name="History" component={History} />
-        </Stack.Navigator>
->>>>>>> 3a2a7be96ec24540a411ef6c24cc7f838d75fcb2
     );
 }
 
