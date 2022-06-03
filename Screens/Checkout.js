4d702062e322a1590fcd85a383b448f27876879a
Russell Quaicoo <russellquaicoo1@gmail.com>
2022-06-03 11:30:41 +0000

    checkout url updated

--- Screens/Checkout.js
+++ Screens/Checkout.js
@@ -96,7 +96,7 @@ export default function Checkout({navigation}) {
 
 const createOrder = () => {
     setLoading(true);
-    fetch('http://127.0.0.1:8000/api/order/', {
+    fetch('https://dryce-staging.herokuapp.com/api/order/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
